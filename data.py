9251f1f24c98af3d730619723aba623a32933fcd
konmue <mail@konrad-mueller.com>
2021-12-06 09:22:57 +0100

    possible to get log data as input

--- data.py
+++ data.py
@@ -12,6 +12,7 @@ class SimulationData(Dataset):
         price_params: dict,
         payoff: Callable,
         payoff_params: dict,
+        take_log: bool = False,
     ) -> None:
         super().__init__()
 
@@ -20,16 +21,24 @@ class SimulationData(Dataset):
         self.x = torch.from_numpy(self.x).float()
 
         self.payoffs = payoff(self.x, **payoff_params)
-
         self.price = pricer(**price_params)
+        self.take_log = take_log
 
     def __len__(self):
         return self.x.shape[0]
 
     def __getitem__(self, idx):
-        return (
-            self.x[:, :-1][idx],
-            self.x.squeeze().diff()[idx],
-            self.payoffs[idx],
-            torch.ones(1) * self.price,
-        )
+        if not self.take_log:
+            return (
+                self.x[:, :-1][idx],
+                self.x.squeeze().diff()[idx],
+                self.payoffs[idx],
+                self.price,
+            )
+        else:
+            return (
+                torch.log(self.x[:, :-1][idx]),
+                self.x.squeeze().diff()[idx],
+                self.payoffs[idx],
+                self.price,
+            )
