b60b98ef134b1046ccc2bfed7357e6941e1943d2
konmue <mail@konrad-mueller.com>
2021-12-10 14:45:04 +0100

    small fix

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 import torch
 from torch.utils.data import Dataset
 from typing import Callable
-import numpy as np import pandas as pd
+import numpy as np
+import pandas as pd
 import dask.dataframe as dd
 
 
