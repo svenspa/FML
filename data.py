ec259121ec98fd36cdab2c5f1054323bcc0e299a
konmue <mail@konrad-mueller.com>
2021-12-10 13:29:26 +0100

    added new dataset class to read from file

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 from typing import Callable
+import pandas as pd
 
 
 class SimulationData(Dataset):
@@ -17,6 +18,46 @@ class SimulationData(Dataset):
         super().__init__()
 
         self.x = generator(**g_params)
+        self.x = self.x.reshape(self.x.shape[0], self.x.shape[1], 1)
+        self.x = torch.from_numpy(self.x).float()
+
+        self.payoffs = payoff(self.x, **payoff_params)
+        self.price = pricer(**price_params)
+        self.take_log = take_log
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx):
+        if not self.take_log:
+            return (
+                self.x[:, :-1][idx],
+                self.x.squeeze().diff()[idx],
+                self.payoffs[idx],
+                self.price,
+            )
+        else:
+            return (
+                torch.log(self.x[:, :-1][idx]),
+                self.x.squeeze().diff()[idx],
+                self.payoffs[idx],
+                self.price,
+            )
+
+
+class DataFromFile(Dataset):
+    def __init__(
+        self,
+        path: str,
+        pricer: Callable,
+        price_params: dict,
+        payoff: Callable,
+        payoff_params: dict,
+        take_log: bool = False,
+    ) -> None:
+        super().__init__()
+
+        self.x = pd.read_parquet(path).values
         self.x = self.x.reshape(self.x.shape[0], self.x.shape[1], 1)
         self.x = torch.from_numpy(self.x).float()
 
