13e94e44d87808cd00eb1084d3ae4cd93c9b4507
konmue <mail@konrad-mueller.com>
2021-12-29 15:42:57 +0100

    delete unnecessary and add attribute to res data

--- data.py
+++ data.py
@@ -8,48 +8,7 @@ import dask.dataframe as dd
 SQRT_252 = 252 ** 0.5
 
 
-class SimulationData(Dataset):
-    def __init__(
-        self,
-        generator: Callable,
-        g_params: dict,
-        pricer: Callable,
-        price_params: dict,
-        payoff: Callable,
-        payoff_params: dict,
-        take_log: bool = False,
-    ) -> None:
-        super().__init__()
-
-        self.x = generator(**g_params)
-        self.x = self.x.reshape(self.x.shape[0], self.x.shape[1], 1)
-        self.x = torch.from_numpy(self.x).float()
-
-        self.payoffs = payoff(self.x, **payoff_params)
-        self.price = pricer(**price_params)
-        self.take_log = take_log
-
-    def __len__(self):
-        return self.x.shape[0]
-
-    def __getitem__(self, idx):
-        if not self.take_log:
-            return (
-                self.x[:, :-1][idx],
-                self.x.squeeze().diff()[idx],
-                self.payoffs[idx],
-                self.price,
-            )
-        else:
-            return (
-                torch.log(self.x[:, :-1][idx]),
-                self.x.squeeze().diff()[idx],
-                self.payoffs[idx],
-                self.price,
-            )
-
-
-class DataFromFolder(Dataset):
+class DataGJR(Dataset):
     def __init__(
         self,
         folder_path: str,
@@ -153,6 +112,7 @@ class DataRes(Dataset):
         self.take_log = take_log
         self.normalize = normalize
         self.S0 = S0
+        self.vol_feature = False
 
     def __len__(self):
         return self.splits
